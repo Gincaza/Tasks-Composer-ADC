ba84c64143929eb1c84e71e7142b84a72837331b
mothnue <dinocruz434@gmail.com>
2025-05-21 17:18:30 +0100

    SCRUM-29 Adicionando tarefas a pagina
    - Formulário pra adicionar uma tarefa criado
    
    - Javascript foi mantido como tag embaixo do arquivo. Não há muito JS complexo pra pagina, nao é necessárioc criar um arquivo externo pra isso

--- core/views.py
+++ core/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
 from .models import Task
 
 def dashboard(request):
     return render(request, 'dashboard.html')
 
+@csrf_exempt
 def tasks(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        due_date = request.POST.get('due_date')
+
+        if title and due_date:
+            Task.objects.create(title=title, description=description, due_date=due_date)
+            return redirect('tasks')
+
     tasks_list = Task.objects.all()
     return render(request, 'tasks.html', {'tasks': tasks_list})
 
