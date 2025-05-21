e538e6d58f5fc9b113ff74e3951eb27d8bbf1025
mothnue <dinocruz434@gmail.com>
2025-05-21 17:53:44 +0100

    SCRUM-30 Editar Tarefa na Página de Tarefas

--- core/views.py
+++ core/views.py
@@ -8,13 +8,21 @@ def dashboard(request):
 @csrf_exempt
 def tasks(request):
     if request.method == 'POST':
+        task_id = request.POST.get('task_id')
         title = request.POST.get('title')
         description = request.POST.get('description')
         due_date = request.POST.get('due_date')
 
-        if title and due_date:
+        if task_id:  # Editar tarefa existente
+            task = Task.objects.get(id=task_id)
+            task.title = title
+            task.description = description
+            task.due_date = due_date
+            task.save()
+        elif title and due_date:  # Criar nova tarefa
             Task.objects.create(title=title, description=description, due_date=due_date)
-            return redirect('tasks')
+
+        return redirect('tasks')
 
     tasks_list = Task.objects.all()
     return render(request, 'tasks.html', {'tasks': tasks_list})
