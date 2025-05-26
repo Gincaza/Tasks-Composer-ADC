6f78c15bb00e52c06f0918f4c0e9739799279cd5
mothnue <dinocruz434@gmail.com>
2025-05-26 19:19:38 +0100

    Revert "Update docusaurus.config.js"
    This reverts commit 988550da4e527a91edc9c8de1d2dad112d819d1f.

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://github.com', // URL do GitHub Pages
+  url: 'https://gincaza.github.io', // URL do GitHub Pages
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/Tasks-Composer-ADC/', // Caminho base do repositório
