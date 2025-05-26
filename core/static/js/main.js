be5dd32529c82f5d63949c2e33157ecd04b09eb1
mothnue <dinocruz434@gmail.com>
2025-05-26 18:04:24 +0100

    Deploy website - based on 9787a9b08800c17a9d59009b966abae35a6b4bdf

--- core/static/js/main.js
+++
@@ -1,7 +0,0 @@
-// Aplicar o tema salvo no localStorage ao carregar a página
-document.addEventListener('DOMContentLoaded', function () {
-    const currentTheme = localStorage.getItem('theme') || 'light';
-    if (currentTheme === 'dark') {
-        document.body.classList.add('dark-theme');
-    }
-});
